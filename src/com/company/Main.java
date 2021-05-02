4b7ba7d3d8f45d7b20f12ba99d3c871e384cd9f1
Natheeshkumar <natheeshkumar.r@thoughtworks.com>
2021-05-02 09:57:32 +0530

    Basic printing

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//int,float,string
+
+        System.out.println("hi");
+        System.out.println(123);
+        System.out.println(123.123);
+
+    }
+}
