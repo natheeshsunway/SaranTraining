07bb3792222313e0d5ab45b99e7ee865ce4d0356
Natheeshkumar <natheeshkumar.r@thoughtworks.com>
2021-05-02 10:50:35 +0530

    Primitive data types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,74 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//int,float,string
 
-        System.out.println("hi");
-        System.out.println(123);
-        System.out.println(123.123);
+/**In c program data types
+ int,
+ float,
+ char,
+ double
+ */
+
+/**In JAVA program data types (PRIMITIVE)
+ int,[*]
+ float,
+ char,
+ double,
+ bool
+ */
+
+//        "I am Natheeshkumar";
+
+/*In c program
+
+char charter=i;
+* */
+        //Variable declaration
+        int numbers = 456383;
+        int number123 = 456383;
+        int _number = 456383;
+        int $number = 456383;
+        int x = 4000;
+
+//I am natheesh
+
+        int number = 456383;
+        char character = 'z';
+        float decimal = 45.67f;
+        boolean state = false;
+
+        char characterArray[] = {'I', ' ', 'a', 'm', ' ', 'n', 'a', 't', 'h', 'e', 'e', 's', 'h'};
+
+        for (int index = 0; index < characterArray.length; index++) {
+            System.out.print(characterArray[index]);
+        }
+        System.out.println();
+
+
+
+        /*In c
+        printf("%d",number);
+        * */
+
+        /*In Java
+          System.out.println(number);
+        * */
+        System.out.println(number);
+        System.out.println(x);
+        System.out.println(decimal);
+        System.out.println(character);
+        System.out.println(state);
+
+
+
+        /*  ARRAY
+         *
+        //index    0, 1 , 2 , 3, 4, 5
+        int a[] = {8, 12, 13, 4, 5, 3};
+
+        System.out.println(a[4]);
+        */
+
 
     }
 }
