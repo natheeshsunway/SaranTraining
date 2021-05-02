9a551a42e18d7e6ff337c2898d1706abfd8bf3aa
Natheeshkumar <natheeshkumar.r@thoughtworks.com>
2021-05-02 13:32:16 +0530

    Non - Primitive data types - String - part - 2 (split)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,37 +5,13 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
+        String message = "Saran,Naveen,Natheesh,lion";
 
-        char a[] = {'a', ' ', '8', '.', '9', '0', '^', 'a', ';', ','};
+        String[] meaasgeArray = message.split(",");
+        System.out.println(meaasgeArray.length);
 
-        for (int index = 0; index < a.length; index++) {
-            System.out.print(a[index]);
+        for (int i = 0; i < meaasgeArray.length; i++) {
+            System.out.println(meaasgeArray[i]);
         }
-
-        System.out.println();
-
-        String message = "Hi ,762@%'{} I am Natheeshkumar,Sarankumar,";
-        System.out.println(message);
-        System.out.println(message.length());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.toLowerCase());
-        System.out.println(message.charAt(5));
-        System.out.println(message.contains("Natheeshkumar"));
-
-        System.out.println();
-
-        message = message.concat("Naveenkumar");
-
-        System.out.println(message.concat("Naveenkumar"));
-
-        if (message.contains("Naveenkumar")) {
-            System.out.println("Natheesh is present");
-        } else {
-            System.out.println("Natheesh is not present");
-        }
-        /*
-        In C,
-        char a[]={'a',' ','8','.','9','0','^'};
-        * */
     }
 }
